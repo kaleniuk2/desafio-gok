4b15e880aa285b9d6ac6d7803c3cb07bf4687267
Guilherme Kaleniuk <guilhermekaleniuk@hotmail.com>
2021-02-09 21:19:19 -0300

    inicio do desafio

---
+++ app/src/main/java/com/kaleniuk2/desafiogo_k/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaleniuk2.desafiogo_k
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
